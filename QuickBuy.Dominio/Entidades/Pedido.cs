99e7cc25bb0d099bfb13d1597638e755082e0188
Fabio Assis <fabio.assis@foursys.com.br>
2020-08-04 15:39:42 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using QuickBuy.Dominio.ObjetosDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+        public int IdUsuario { get; set; }
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+        public DateTime DataPrevisaoEntrega  { get; set; }
+        public string CEP { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroDoEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+        public decimal Preco { get; set; }
+
+
+    }
+
+}
